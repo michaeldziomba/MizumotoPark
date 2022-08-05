453809719be107e282c9b0c3b7b2befe2a57edec
Michael Dziomba <mdziomba@gmail.com>
2022-08-05 22:32:48 +0900

    First commit to a lifetime commitment

---
+++ src/main/java/app/EntryPoint.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Hello world!
+ *
+ */
+public class EntryPoint
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Mizumoto Park!" );
+    }
+}
